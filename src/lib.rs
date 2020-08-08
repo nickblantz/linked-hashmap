aa404f68181a2ded8a4a7a444305e7ed806f41d0
Nick Blantz <nicholasblantz@gmail.com>
2020-08-08 14:13:15 -0400

    Adding Entry API

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
-use std::borrow::Borrow;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::mem;
+use std::{
+    borrow::Borrow,
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+    mem,
+    ops::Index,
+};
 
 const INITIAL_BUCKETS: usize = 1;
 const BUCKET_SCALE_FACTOR: usize = 2;
+const RESIZE_NUM: usize = 3;
+const RESIZE_DEN: usize = 4;
+const fn resize_pred(items: usize, buckets: usize) -> bool {
+    items >= RESIZE_NUM * buckets / RESIZE_DEN
+}
 
 #[derive(Debug)]
 pub struct HashMap<K, V> {
@@ -26,10 +34,9 @@ where
     K: Hash + PartialEq + Eq,
 {
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        if self.items >= 3 * self.buckets.len() / 4 {
+        if resize_pred(self.items, self.buckets.len()) {
             self.resize();
         }
-
         let bucket = self.bucket(&key);
         let bucket = &mut self.buckets[bucket];
         match bucket.iter_mut().find(|(e_key, _)| *e_key == key) {
@@ -42,17 +49,31 @@ where
         }
     }
 
-    // pub fn get(&self, key: &K) -> Option<&V> {
-    //     self.buckets[self.bucket(&key)]
-    //         .iter()
-    //         .find(|(e_k, _)| e_k == key)
-    //         .map(|&(_, ref v)| v)
-    // }
+    pub fn entry(&mut self, key: K) -> Entry<K, V> {
+        if resize_pred(self.items, self.buckets.len()) {
+            self.resize();
+        }
+        let bucket = self.bucket(&key);
+        if let Some(entry) = self.buckets[bucket]
+            .iter_mut()
+            .find(|&&mut (ref e_k, _)| e_k == &key)
+        {
+            Entry::Occupied(OccupiedEntry {
+                entry: unsafe { &mut *(entry as *mut _) },
+            })
+        } else {
+            Entry::Vacant(VacantEntry {
+                key: key,
+                map: self,
+                bucket,
+            })
+        }
+    }
 
     pub fn get<Q>(&self, key: &Q) -> Option<&V>
     where
         K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized
+        Q: Hash + Eq + ?Sized,
     {
         self.buckets[self.bucket(key.borrow())]
             .iter()
@@ -63,7 +84,7 @@ where
     pub fn remove<Q>(&mut self, key: &Q) -> Option<V>
     where
         K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized
+        Q: Hash + Eq + ?Sized,
     {
         let bucket = self.bucket(&key);
         let bucket = &mut self.buckets[bucket];
@@ -79,7 +100,7 @@ where
     pub fn contains_key<Q>(&self, key: &Q) -> bool
     where
         K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized
+        Q: Hash + Eq + ?Sized,
     {
         self.get(&key).is_some()
     }
@@ -89,7 +110,6 @@ where
             0 => INITIAL_BUCKETS,
             x => x * BUCKET_SCALE_FACTOR,
         };
-
         let mut new_buckets = Vec::with_capacity(target_size);
         new_buckets.extend((0..target_size).map(|_| Vec::new()));
         self.buckets
@@ -107,11 +127,23 @@ where
     fn bucket<Q>(&self, key: &Q) -> usize
     where
         K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized
+        Q: Hash + Eq + ?Sized,
     {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         (hasher.finish() % self.buckets.len() as u64) as usize
+    }
+}
+
+impl<'a, K, Q, V> Index<&'a Q> for HashMap<K, V>
+where
+    K: Eq + Hash + Borrow<Q>,
+    Q: Eq + Hash + ?Sized,
+{
+    type Output = V;
+
+    fn index(&self, index: &Q) -> &Self::Output {
+        self.get(index).expect("no entry found for key")
     }
 }
 
@@ -121,30 +153,21 @@ pub struct HMIter<'a, K: 'a, V: 'a> {
     item: usize,
 }
 
-// impl <'a, K, V> for HMIter<'a, K: 'a, V: 'a> {
-//     pub fn new() -> Self {
-//         HMIter {
-//         }
-//     }
-// }
-
 impl<'a, K, V> Iterator for HMIter<'a, K, V> {
     type Item = (&'a K, &'a V);
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             match self.map.buckets.get(self.bucket) {
-                Some(bucket) => {
-                    match bucket.get(self.item) {
-                        Some(&(ref k, ref v)) => {
-                            self.item += 1;
-                            break Some((k, v));
-                        },
-                        None => {
-                            self.bucket += 1;
-                            self.item = 0;
-                            continue;
-                        },
+                Some(bucket) => match bucket.get(self.item) {
+                    Some(&(ref k, ref v)) => {
+                        self.item += 1;
+                        break Some((k, v));
+                    }
+                    None => {
+                        self.bucket += 1;
+                        self.item = 0;
+                        continue;
                     }
                 },
                 None => break None,
@@ -158,7 +181,69 @@ impl<'a, K, V> IntoIterator for &'a HashMap<K, V> {
     type IntoIter = HMIter<'a, K, V>;
 
     fn into_iter(self) -> <Self as IntoIterator>::IntoIter {
-        HMIter { map: self, bucket: 0, item: 0 }
+        HMIter {
+            map: self,
+            bucket: 0,
+            item: 0,
+        }
+    }
+}
+
+pub struct OccupiedEntry<'a, K: 'a, V: 'a> {
+    entry: &'a mut (K, V),
+}
+
+pub struct VacantEntry<'a, K: 'a, V: 'a> {
+    key: K,
+    map: &'a mut HashMap<K, V>,
+    bucket: usize,
+}
+
+impl<'a, K, V> VacantEntry<'a, K, V>
+where
+    K: Eq + Hash,
+{
+    fn insert(self, value: V) -> &'a mut V {
+        self.map.buckets[self.bucket].push((self.key, value));
+        self.map.items += 1;
+        &mut self.map.buckets[self.bucket].last_mut().unwrap().1
+    }
+}
+
+pub enum Entry<'a, K: 'a, V: 'a> {
+    Occupied(OccupiedEntry<'a, K, V>),
+    Vacant(VacantEntry<'a, K, V>),
+}
+
+impl<'a, K, V> Entry<'a, K, V>
+where
+    K: Eq + Hash,
+{
+    pub fn or_insert(self, value: V) -> &'a mut V {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(value),
+        }
+    }
+
+    pub fn or_insert_with<F>(self, default: F) -> &'a mut V
+    where
+        F: FnOnce() -> V,
+    {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(default()),
+        }
+    }
+
+    pub fn or_default(self) -> &'a mut V
+    where
+        V: Default,
+    {
+        match self {
+            Entry::Occupied(e) => &mut e.entry.1,
+            Entry::Vacant(e) => e.insert(V::default()),
+        }
     }
 }
 
@@ -167,7 +252,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn insert() {
+    fn hashmap_insert() {
         // Setup
         let mut map = HashMap::new();
 
@@ -181,7 +266,7 @@ mod tests {
     }
 
     #[test]
-    fn get() {
+    fn hashmap_get() {
         // Setup
         let mut map = HashMap::new();
         map.insert(0, "a");
@@ -202,7 +287,7 @@ mod tests {
     }
 
     #[test]
-    fn remove() {
+    fn hashmap_remove() {
         // Setup
         let mut map = HashMap::new();
         map.insert(0, "a");
@@ -219,7 +304,7 @@ mod tests {
     }
 
     #[test]
-    fn len() {
+    fn hashmap_len() {
         // Setup
         let mut map = HashMap::new();
 
@@ -243,7 +328,7 @@ mod tests {
     }
 
     #[test]
-    fn contains_key() {
+    fn hashmap_contains_key() {
         // Setup
         let mut map = HashMap::new();
         map.insert(0, "a");
@@ -260,13 +345,14 @@ mod tests {
     }
 
     #[test]
-    fn into_iter() {
+    fn hashmap_into_iter() {
         // Setup
         let mut map = HashMap::new();
         map.insert(0, "a");
         map.insert(1, "b");
         map.insert(2, "c");
 
+        // Scenario: I can index into my hashmap
         for (&k, &v) in &map {
             match k {
                 0 => assert_eq!(v, "a"),
@@ -275,5 +361,39 @@ mod tests {
                 _ => unreachable!(),
             }
         }
+    }
+
+    #[test]
+    fn entry_or_insert() {
+        // Setup
+        let mut map: HashMap<&str, u32> = HashMap::new();
+
+        // Scenario: Insert a new entry
+        map.entry("poneyland").or_insert(3);
+        assert_eq!(map["poneyland"], 3);
+
+        // Scenario: Modify an existing entry
+        *map.entry("poneyland").or_insert(10) *= 2;
+        assert_eq!(map["poneyland"], 6);
+    }
+
+    #[test]
+    fn entry_or_insert_with() {
+        // Setup
+        let mut map: HashMap<&str, String> = HashMap::new();
+        let s = "hoho".to_string();
+
+        // Scenario: Insert a new entry with a closure
+        map.entry("poneyland").or_insert_with(|| s);
+        assert_eq!(map["poneyland"], "hoho".to_string());
+    }
+
+    #[test]
+    fn entry_or_default() {
+        let mut map: HashMap<&str, Option<u32>> = HashMap::new();
+
+        // Scenario: Insert a new entry with a default value
+        map.entry("poneyland").or_default();
+        assert_eq!(map["poneyland"], None);
     }
 }
